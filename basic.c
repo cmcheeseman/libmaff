1fa161989c6fa3c5cce8113466397dcf6e9c5b1b
cmcheeseman <104270227+cmcheeseman@users.noreply.github.com>
2024-01-09 18:00:09 -0600

    Add files via upload

---
+++ basic.c
@@ -0,0 +1,17 @@
+#include "maff.h"
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+int mul(int a, int b) {
+  return a * b;
+}
+
+double divi(int a, int b) {
+  return a / b;
+}
